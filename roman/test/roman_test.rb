81f9b301d72c03f2ca3ae2cdb47e1536e54f9b56
Pavan <pavankumar@railsfactory.org>
2014-07-04 10:24:29 +0530

    Done with the logic, some errors need to be corrected

--- roman/test/roman_test.rb
+++ roman/test/roman_test.rb
@@ -4,11 +4,6 @@ require 'test/unit'
 
 class TestRoman < Test::Unit::TestCase
 
-def test_hello
-assert true
-end
-
-
 def test_one
 assert_equal 'I',Roman.convert(1)
 assert_equal 'II',Roman.convert(2)
@@ -19,5 +14,8 @@ assert_equal 'VI',Roman.convert(6)
 assert_equal 'VII',Roman.convert(7)
 assert_equal 'IX',Roman.convert(9)
 assert_equal 'X',Roman.convert(10)
+assert_equal 'CX',Roman.convert(110)
+assert_equal 'CXXXVII',Roman.convert(137)
+assert_equal 'CCX',Roman.convert(210)
 end
 end  
