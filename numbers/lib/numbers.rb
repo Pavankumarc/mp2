39e902eee7915d3f390507e6ebcd043c791cabd2
Pavan <pavankumar@railsfactory.org>
2014-07-07 12:28:54 +0530

    test successfull for all the given inputs

--- numbers/lib/numbers.rb
+++ numbers/lib/numbers.rb
@@ -1,12 +1,25 @@
 class Numbers
 
-  def self.convert(number)
-    if number ==1
-    '1st'
-  else
-    '2nd'
-  end
-  
-  end
+def self.convert(n)
+if n < 10
+s=comp(n)
+return "#{n}"+s
+else
+m=n%10
+s=comp(m)
+return "#{n}"+s
+end    
+end
 
+def self.comp(number)
+if number ==1
+'st'
+elsif number ==2
+'nd'
+elsif number ==3
+'rd'
+else
+'th'
+end
+end
 end 
--- numbers/test/numbers_test.rb
+++ numbers/test/numbers_test.rb
@@ -6,9 +6,18 @@ class TestNumbers < Test::Unit::TestCase
 
 
 
-  def test_num
-    assert_equal '1st',Numbers.convert(1)
-    assert_equal '2nd',Numbers.convert(2)
+def test_num
+assert_equal '1st',Numbers.convert(1)
+assert_equal '2nd',Numbers.convert(2)
+assert_equal '3rd',Numbers.convert(3)
+assert_equal '4th',Numbers.convert(4)
+assert_equal '5th',Numbers.convert(5)
+assert_equal '31st',Numbers.convert(31)
+assert_equal '28th',Numbers.convert(28)
+assert_equal '10th',Numbers.convert(10)
+assert_equal '16th',Numbers.convert(16)
+assert_equal '23rd',Numbers.convert(23)
+assert_equal '22nd',Numbers.convert(22)
   end
 
 
