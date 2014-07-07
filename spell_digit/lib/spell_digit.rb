e499251c5fd25525ad8d0ac599852a13ad6caf93
Pavan <pavankumar@railsfactory.org>
2014-07-07 12:14:19 +0530

    test successfull for given user inputs upto one crore

--- spell_digit/lib/spell_digit.rb
+++ spell_digit/lib/spell_digit.rb
@@ -1,58 +1,146 @@
 class Spell_digit
 
-
-
 def self.convert(n)
 if n>0 and n<10
+a=ones(n)
+elsif n>9 and n<100
+a=tens(n)
+elsif n>99 and n<1000
+a=hundreds(n)
+elsif n>999 and n<10000
+a=thousands(n)
+elsif n>9999 and n<100000
+a=tthousands(n)
+elsif n>99999 and n<1000000
+a=lakhs(n)
+elsif n>999999 and n<10000000
+a=tlakhs(n)
+else n==10000000
+a="one crore"
+end
+return a
+end
+
+
+def self.ones(n)
 s1 = comp(n) 
 return s1
+end
 
-elsif n>9 and n<100
+def self.tens(n)
+if n%10==0 
+s1=comp(n)
+return s1
+elsif n>10 and n<20
+s1=comp(n)
+return s1
+else
 m=n%10
 s1=comp(m)
 n=n-m
 s2=comp(n)
 return s2+s1
+end
+end
 
-elsif n>99 and n<1000
-m=n%10
-s1=comp(m)
-n=n-m
-p=n%100
-s2=comp(p)
-n=n-p
+def self.hundreds(n)
+if n%100==0
+a=n%100
+s=tens(a)
+n=n-a
+n=n/100
+s4=" hundred"
+s5=comp(n)
+return s5+s4+s
+else
+a=n%100
+s=tens(a)
+n=n-a
+if n==0
+return s
+else
 n=n/100
 s3=" and "
 s4=" hundred"
 s5=comp(n)
-return s5+s4+s3+s2+s1
-
-elsif n>999 and n<10000
-a=n%10
-s1=comp(a)
-n=n-a
-b=n%100
-s2=comp(b)
-n=n-a
-n=n/100
-s=" and "
-s3=" hundred"
-c=n%10
-s4=comp(c)
-n=n-c
-n=n/10
-s5=" thousand "
-s6=comp(n)
-return s6+s5+s4+s3+s+s2+s1
-
-
-
+return s5+s4+s3+s
 end
 end
+end
+
+def self.thousands(n)
+if n%1000==0
+n=n/1000
+s1=comp(n)
+s2="thousand"
+return s1+s2
+else
+a=n%1000
+s=hundreds(a)
+n=n-a
+n=n/1000
+s1=comp(n)
+s2=" thousand "
+return s1+s2+s
+end
+end
+
+def self.tthousands(n)
+if n%10000==0
+n=n/10000
+s1=comp(n)
+s2="thousand"
+return s1+s2
+else
+a=n%1000
+s=hundreds(a)
+n=n-a
+n=n/1000
+s1=tens(n)
+s2="thousand"
+return s1+s2+s
+end
+end
+
+def self.lakhs(n)
+if n%100000==0
+n=n/100000
+s1=comp(n)
+s2="lakh"
+return s1+s2
+else
+a=n%100000
+s=tthousands(a)
+n=n-a
+n=n/100000
+s1=comp(n)
+s2=" lakh "
+return s1+s2+s
+end
+end
+
+def self.tlakhs(n)
+if n%1000000==0
+n=n/1000000
+s1=comp(n)
+s2="lakhs"
+return s1+s2
+else
+a=n%100000
+s=tthousand(a)
+n=n-a
+n=n/100000
+s1=comp(n)
+s2=" lakhs "
+return s1+s2+s
+end
+end
+
 
 def self.comp(number)
-
-if number == 1
+if number ==0
+""
+elsif number == 1
 "one"
 elsif number ==2
 "two"
@@ -107,7 +195,5 @@ elsif number ==80
 else number ==90
 "ninty"
 end
-
 end
-
 end
--- spell_digit/test/spell_digit_test.rb
+++ spell_digit/test/spell_digit_test.rb
@@ -10,7 +10,15 @@ class TestSpell_digit < Test::Unit::TestCase
 
 
   def test_one
-   assert_equal "two hundred and thirtyeight",Spell_digit.convert(238)
-   assert_equal "nine thousand nine hundred and nintynine",Spell_digit.convert(9999)
+   assert_equal "eleven",Spell_digit.convert(11)
+   assert_equal "twelve",Spell_digit.convert(12)
+   assert_equal "fifteen",Spell_digit.convert(15)
+   assert_equal "thirtythree",Spell_digit.convert(33)
+   assert_equal "five hundred",Spell_digit.convert(500)
+   assert_equal "six hundred and eightyseven",Spell_digit.convert(687)
+   assert_equal "one thousand two hundred and fifty",Spell_digit.convert(1250)
+   assert_equal "two thousand three hundred and fifty",Spell_digit.convert(2350)
+   assert_equal "five thousand eight hundred and ninty",Spell_digit.convert(5890)
+   assert_equal "eight thousand ninty",Spell_digit.convert(8090)
   end
 end
