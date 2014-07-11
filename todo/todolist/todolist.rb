d405e55c77a343e8beab7a7bf674bcd09ba22d31
Pavan <pavankumar@railsfactory.org>
2014-07-11 17:11:19 +0530

    tested successfull for possible 36 assertions

--- todo/todolist/todolist.rb
+++ todo/todolist/todolist.rb
@@ -1,73 +1,69 @@
 class Todolist
-attr_accessor :filename,:todo,:pending,:completed
-$todo=[]
-$pending=[]
-$completed=[]
+
 
 #initialize method
 def initialize(filename)
-$filename=filename
+@filename=filename
 end
 
 #empty method
 def empty
-$todo.clear
-$pending.clear
-$completed.clear
-return "clear"
+@todo=[]
+@pending=[]
+@completed=[]
+
 end
 
 #add method
 def add(item)
-$pending << item
-return $pending.size
+@pending << item
+return @pending
 end
 
 #pending method
 def pending
-return $pending.size
+return @pending
 end
 
 #complete method
 def complete(linenumber)
-$completed << $pending[linenumber-1]
-$pending.delete_at(linenumber - 1)
-return $completed.size
+@completed << @pending[linenumber-1]
+@pending.delete_at(linenumber - 1)
+return @completed
 end
 
 #completed method
 def completed
-return $completed.size
+return @completed
 end
 
 #list method
 def list
-$todo = $pending + $completed
-return $todo.size
+@todo = @pending + @completed
+return @todo
 end
 
 #delete method
 def delete(linenumber)
-$completed.delete_at(linenumber - 1)
-return $completed.size
+@completed.delete_at(linenumber - 1)
+return @completed
 end
 
 #modify method
 def modify(linenumber,item)
-$pending.delete_at(linenumber-1)
-$pending << item
-return $pending.size
+@pending[linenumber-1].replace(item)
+return @pending
 end
 
 
 #show pending method
 def show_pending(linenumber)
-return $pending[linenumber-1]
+return @pending[linenumber-1]
 end
 
 #show completed method
 def show_completed(linenumber)
-return $completed[linenumber-1]
+return @completed[linenumber-1]
 end
 end
 
--- todo/todolist/todolist_test.rb
+++ todo/todolist/todolist_test.rb
@@ -4,74 +4,158 @@ require 'test/unit'
 
 
 class Testcase < Test::Unit::TestCase
-attr_accessor :t
-$t=Todolist.new("a.txt")
+
+def setup
+@t=Todolist.new("a.txt")
+end
+
+def teardown
+@t=nil
+end
 
 #testing for empty file
-def test_1
-assert_equal "clear",$t.empty
-assert_equal 0,$t.pending
-assert_equal 0,$t.completed
-assert_equal 0,$t.list
+def test_empty
+@t.empty
+assert_equal 0,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 0,@t.list.size
 end
 
 #testing for first add method  
-def test_2
-assert_equal 1,$t.add("hello")
-assert_equal 2,$t.add("hey")
-assert_equal 2,$t.pending
-assert_equal "hey",$t.show_pending(2)
+def test_add1
+
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+@t.add("bye")
+@t.add("tc")
+assert_equal 5,@t.pending.size
+assert_equal 5,@t.list.size
+assert_equal 0,@t.completed.size
+assert_equal "hello",@t.show_pending(2)
 end
 
-#testing second add method
-def test_3
-assert_equal 3,$t.add("hi")
-assert_equal 4,$t.add("bye")
-assert_equal 5,$t.add("tc")
-assert_equal 5,$t.pending
-end
 
 #testing for complete
-def test_4
-assert_equal 5,$t.pending
-assert_equal 1,$t.complete(1)
-assert_equal 2,$t.complete(2)
-assert_equal 3,$t.complete(3)
-assert_equal "hello",$t.show_completed(1)
-assert_equal 2,$t.pending
-assert_equal 3,$t.completed
+def test_complete
+
+#before calling method
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+assert_equal 3,@t.pending.size
+assert_equal 3,@t.list.size
+assert_equal 0,@t.completed.size
+
+#action
+@t.complete(1)
+@t.complete(2)
+
+#after calling method
+assert_equal 3,@t.list.size
+assert_equal 1,@t.pending.size
+assert_equal 2,@t.completed.size
 end
 
  
 #testing for pending
-def test_5
-assert_equal 2,$t.pending                
+def test_pending
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+assert_equal 3,@t.pending.size
+assert_equal 3,@t.list.size
+assert_equal 0,@t.completed.size
+assert_equal "hi",@t.show_pending(1)
+assert_equal "hello",@t.show_pending(2)
+assert_equal "hey",@t.show_pending(3)
 end
+
+
 
 #testing for delete
-def test_6
-assert_equal 2,$t.delete(1)
-assert_equal 2,$t.completed
+def test_delete
+
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+assert_equal 3,@t.pending.size
+assert_equal 3,@t.list.size
+assert_equal 0,@t.completed.size
+
+
+@t.complete(1)
+assert_equal 2,@t.pending.size
+assert_equal 1,@t.completed.size
+
+@t.delete(1)
+
+assert_equal 2,@t.pending.size
+assert_equal 0,@t.completed.size
+assert_equal 2,@t.list.size
 end
 
+
+
 #testing for todo list
-def test_7
-assert_equal 4,$t.list
+def test_list
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+@t.list
+assert_equal 3,@t.list.size
 end
+
+
+
 
 #testing for modify
 def test_8
-assert_equal 2,$t.modify(1,"bye")
+
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+assert_equal 3,@t.pending.size
+assert_equal 3,@t.list.size
+assert_equal 0,@t.completed.size
+assert_equal "hi",@t.show_pending(1)
+
+@t.modify(1,"bye")
+@t.modify(2,"hi")
+assert_equal "bye",@t.show_pending(1)
+assert_equal "hi",@t.show_pending(2)
 end
+
+
 
 #testing for show pending
 def test_9
-assert_equal "bye",$t.show_pending(1)
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+assert_equal "hey",@t.show_pending(3)
 end
+
 
 #testing for show completed
 def test_show
-assert_equal "hi",$t.show_completed(1)
+
+@t.empty
+@t.add("hi")
+@t.add("hello")
+@t.add("hey")
+
+@t.complete(1)
+@t.complete(3)
+assert_equal "hi",@t.show_completed(1)
+
 end
 
 end
