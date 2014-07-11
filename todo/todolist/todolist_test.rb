79cf4b90335d975d98a81b6bc236e4d4ecfc1862
Pavan <pavankumar@railsfactory.org>
2014-07-11 14:20:37 +0530

    tested successfull for possible 25 assertions using object to call the function

--- todo/todolist/todolist_test.rb
+++ todo/todolist/todolist_test.rb
@@ -2,69 +2,72 @@ require './todolist'
 require 'test/unit'
 
 
+
 class Testcase < Test::Unit::TestCase
+attr_accessor :t
+$t=Todolist.new("a.txt")
 
 def test_1
-assert_equal "clear",Todolist.empty
-assert_equal 0,Todolist.pending
-assert_equal 0,Todolist.completed
-assert_equal 0,Todolist.list
+assert_equal "clear",$t.empty
+assert_equal 0,$t.pending
+assert_equal 0,$t.completed
+assert_equal 0,$t.list
 end
 
   
 def test_2
-assert_equal 1,Todolist.add("hello")
-assert_equal 2,Todolist.add("hey")
-assert_equal 2,Todolist.pending
-assert_equal "hey",Todolist.show_pending(2)
+assert_equal 1,$t.add("hello")
+assert_equal 2,$t.add("hey")
+assert_equal 2,$t.pending
+assert_equal "hey",$t.show_pending(2)
 end
 
 def test_3
-assert_equal 3,Todolist.add("hi")
-assert_equal 4,Todolist.add("bye")
-assert_equal 5,Todolist.add("tc")
-assert_equal 5,Todolist.pending
+assert_equal 3,$t.add("hi")
+assert_equal 4,$t.add("bye")
+assert_equal 5,$t.add("tc")
+assert_equal 5,$t.pending
 end
 
 
 def test_4
-assert_equal 5,Todolist.pending
-assert_equal 1,Todolist.complete(1)
-assert_equal 2,Todolist.complete(2)
-assert_equal 3,Todolist.complete(3)
-assert_equal "hello",Todolist.show_completed(1)
-assert_equal 2,Todolist.pending
-assert_equal 3,Todolist.completed
+assert_equal 5,$t.pending
+assert_equal 1,$t.complete(1)
+assert_equal 2,$t.complete(2)
+assert_equal 3,$t.complete(3)
+assert_equal "hello",$t.show_completed(1)
+assert_equal 2,$t.pending
+assert_equal 3,$t.completed
 end
 
  
 
 def test_5
-puts Todolist.pending
-assert_equal 2,Todolist.pending                
+assert_equal 2,$t.pending                
 end
 
 
 def test_6
-assert_equal 2,Todolist.delete(1)
-assert_equal 2,Todolist.completed
+assert_equal 2,$t.delete(1)
+assert_equal 2,$t.completed
 end
 
 
 def test_7
-assert_equal 4,Todolist.list
+assert_equal 4,$t.list
 end
 
 
 def test_8
-assert_equal 2,Todolist.modify(1,"bye")
+assert_equal 2,$t.modify(1,"bye")
 end
 
 def test_9
-assert_equal "bye",Todolist.show_pending(1)
+assert_equal "bye",$t.show_pending(1)
 end
 
 
 
 
 end
+
