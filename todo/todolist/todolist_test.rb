ea4eac44b6c95c2e23235bcf419e7bb1cc92464f
Pavan <pavankumar@railsfactory.org>
2014-07-11 12:08:38 +0530

    tested successfull for possible 25 assertions

--- todo/todolist/todolist_test.rb
+++ todo/todolist/todolist_test.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 
 class Testcase < Test::Unit::TestCase
 
-def test_empty
+def test_1
 assert_equal "clear",Todolist.empty
 assert_equal 0,Todolist.pending
 assert_equal 0,Todolist.completed
@@ -12,60 +12,58 @@ assert_equal 0,Todolist.list
 end
 
   
-def test_add1
+def test_2
 assert_equal 1,Todolist.add("hello")
 assert_equal 2,Todolist.add("hey")
 assert_equal 2,Todolist.pending
 assert_equal "hey",Todolist.show_pending(2)
 end
 
-def test_add2
+def test_3
 assert_equal 3,Todolist.add("hi")
-assert_equal 4,Todolist.add("hi")
-assert_equal 5,Todolist.add("hi")
+assert_equal 4,Todolist.add("bye")
+assert_equal 5,Todolist.add("tc")
 assert_equal 5,Todolist.pending
 end
 
 
-def test_add3
+def test_4
 assert_equal 5,Todolist.pending
 assert_equal 1,Todolist.complete(1)
 assert_equal 2,Todolist.complete(2)
 assert_equal 3,Todolist.complete(3)
 assert_equal "hello",Todolist.show_completed(1)
 assert_equal 2,Todolist.pending
+assert_equal 3,Todolist.completed
 end
 
  
 
-def test_add4
+def test_5
 puts Todolist.pending
 assert_equal 2,Todolist.pending                
 end
 
 
-def test_add5
+def test_6
 assert_equal 2,Todolist.delete(1)
 assert_equal 2,Todolist.completed
 end
 
 
-def test_add6
+def test_7
 assert_equal 4,Todolist.list
 end
 
 
-def test_add7
-assert_equal 2,Todolist.modify(2,"bye")
+def test_8
+assert_equal 2,Todolist.modify(1,"bye")
 end
 
-def test_add8
-assert_equal "bye",Todolist.show_pending(2)
+def test_9
+assert_equal "bye",Todolist.show_pending(1)
 end
 
-def test_add9
-assert_equal "hi",Todolist.show_completed(1)
-end
 
 
 
