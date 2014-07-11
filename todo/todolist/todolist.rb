9c4dd6663c34c3ed599a783826ff381399a3c7fb
Pavan <pavankumar@railsfactory.org>
2014-07-11 15:05:16 +0530

    tested successfull for possible 25 assertions using object to call the function with comment lines

--- todo/todolist/todolist.rb
+++ todo/todolist/todolist.rb
@@ -4,10 +4,12 @@ $todo=[]
 $pending=[]
 $completed=[]
 
+#initialize method
 def initialize(filename)
 $filename=filename
 end
 
+#empty method
 def empty
 $todo.clear
 $pending.clear
@@ -15,37 +17,42 @@ $completed.clear
 return "clear"
 end
 
+#add method
 def add(item)
 $pending << item
 return $pending.size
 end
 
+#pending method
 def pending
 return $pending.size
 end
 
+#complete method
 def complete(linenumber)
 $completed << $pending[linenumber-1]
 $pending.delete_at(linenumber - 1)
 return $completed.size
 end
 
+#completed method
 def completed
 return $completed.size
 end
 
+#list method
 def list
 $todo = $pending + $completed
 return $todo.size
 end
 
-
+#delete method
 def delete(linenumber)
 $completed.delete_at(linenumber - 1)
 return $completed.size
 end
 
-
+#modify method
 def modify(linenumber,item)
 $pending.delete_at(linenumber-1)
 $pending << item
@@ -53,11 +60,12 @@ return $pending.size
 end
 
 
-
+#show pending method
 def show_pending(linenumber)
 return $pending[linenumber-1]
 end
 
+#show completed method
 def show_completed(linenumber)
 return $completed[linenumber-1]
 end
--- todo/todolist/todolist_test.rb
+++ todo/todolist/todolist_test.rb
@@ -7,6 +7,7 @@ class Testcase < Test::Unit::TestCase
 attr_accessor :t
 $t=Todolist.new("a.txt")
 
+#testing for empty file
 def test_1
 assert_equal "clear",$t.empty
 assert_equal 0,$t.pending
@@ -14,7 +15,7 @@ assert_equal 0,$t.completed
 assert_equal 0,$t.list
 end
 
-  
+#testing for first add method  
 def test_2
 assert_equal 1,$t.add("hello")
 assert_equal 2,$t.add("hey")
@@ -22,6 +23,7 @@ assert_equal 2,$t.pending
 assert_equal "hey",$t.show_pending(2)
 end
 
+#testing second add method
 def test_3
 assert_equal 3,$t.add("hi")
 assert_equal 4,$t.add("bye")
@@ -29,7 +31,7 @@ assert_equal 5,$t.add("tc")
 assert_equal 5,$t.pending
 end
 
-
+#testing for complete
 def test_4
 assert_equal 5,$t.pending
 assert_equal 1,$t.complete(1)
@@ -41,33 +43,36 @@ assert_equal 3,$t.completed
 end
 
  
-
+#testing for pending
 def test_5
 assert_equal 2,$t.pending                
 end
 
-
+#testing for delete
 def test_6
 assert_equal 2,$t.delete(1)
 assert_equal 2,$t.completed
 end
 
-
+#testing for todo list
 def test_7
 assert_equal 4,$t.list
 end
 
-
+#testing for modify
 def test_8
 assert_equal 2,$t.modify(1,"bye")
 end
 
+#testing for show pending
 def test_9
 assert_equal "bye",$t.show_pending(1)
 end
 
-
-
+#testing for show completed
+def test_show
+assert_equal "hi",$t.show_completed(1)
+end
 
 end
 
